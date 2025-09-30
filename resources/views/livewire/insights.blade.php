0c05cb5c66369742b352670979152c390dc7172d
skjaguar2030 <104493341+skjaguar2030@users.noreply.github.com>
2025-09-30 12:37:45 +0200

    Added the Insights page

---
+++ resources/views/livewire/insights.blade.php
@@ -0,0 +1,175 @@
+<div>
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
+    <div class="py-4">
+      <p class="text-sky-500 text-sm md:text-3xl font-bold mb-4">
+        insights
+      </p>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-6 gap-4">
+      <!-- Employers Section -->
+      <div class="py-4 md:py-8 col-span-1 md:col-span-2 border-t border-gray-300">
+        <div class="grid grid-rows-1 md:grid-rows-6 gap-y-2 md:gap-y-8">
+          <div class="row-span-1 md:row-span-5">
+            <h2 class="text-sm md:text-xl mb-4">September 26, 2025 | United States | Media mentions</h2>
+            <p class="text-black-700 font-bold md:text-4xl">
+            Business Insider: Trump’s $100,000 Visa Fee Threatens Wall Street’s Pipeline of Junior Bankers and...
+            </p>
+          </div>
+          <div class="row-span-1 md:row-span-1">
+            <a href="#" class="text-sky-500 hover:underline text-sm font-medium md:text-2xl inline-flex items-center">
+              Read more 
+              <span class="ml-1">→</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- Individuals Section -->
+      <div class="py-4 md:py-8 col-span-1 md:col-span-2 border-t border-gray-300">
+        <div class="grid grid-rows-1 md:grid-rows-6 gap-y-2 md:gap-y-8">
+          <div class="row-span-1 md:row-span-5">
+            <h2 class="text-sm md:text-xl mb-4">September 25, 2025 | Saudi Arabia | Media mentions</h2>
+            <p class="text-black-700 font-bold md:text-4xl">
+          Asharq Al Awsat: السعودية تمضي مع «رؤية 2030» بدعم التصنيع والتوطين وتوسع الاقتصاد
+            </p>
+          </div>
+          <div class="row-span-1 md:row-span-1">
+            <a href="#" class="text-sky-500 hover:underline text-sm font-medium md:text-2xl inline-flex items-center">
+              Read more 
+              <span class="ml-1">→</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Individuals Section -->
+     <div class="py-4 md:py-8 col-span-1 md:col-span-2 border-t border-gray-300">
+        <div class="grid grid-rows-1 md:grid-rows-6 gap-y-2 md:gap-y-8">
+          <div class="row-span-1 md:row-span-5">
+            <h2 class="text-sm md:text-xl mb-4">September 25, 2025 | United Kingdom | Fragomen news</h2>
+            <p class="text-black-700 font-bold md:text-4xl">
+          Fragomen Spotlighted in Financial Times for 2025 Innovative Lawyers Europe Awards
+            </p>
+          </div>
+          <div class="row-span-1 md:row-span-1">
+            <a href="#" class="text-sky-500 hover:underline text-sm font-medium md:text-2xl inline-flex items-center">
+              Read more 
+              <span class="ml-1">→</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <!----------- Divider ----------->
+
+      <!-- Employers Section -->
+      <div class="py-4 md:py-8 col-span-1 md:col-span-2 border-t border-gray-300">
+        <div class="grid grid-rows-1 md:grid-rows-6 gap-y-2 md:gap-y-8">
+          <div class="row-span-1 md:row-span-5">
+            <h2 class="text-sm md:text-xl mb-4">September 26, 2025 | United States | Media mentions</h2>
+            <p class="text-black-700 font-bold md:text-4xl">
+            Business Insider: Trump’s $100,000 Visa Fee Threatens Wall Street’s Pipeline of Junior Bankers and...
+            </p>
+          </div>
+          <div class="row-span-1 md:row-span-1">
+            <a href="#" class="text-sky-500 hover:underline text-sm font-medium md:text-2xl inline-flex items-center">
+              Read more 
+              <span class="ml-1">→</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- Individuals Section -->
+      <div class="py-4 md:py-8 col-span-1 md:col-span-2 border-t border-gray-300">
+        <div class="grid grid-rows-1 md:grid-rows-6 gap-y-2 md:gap-y-8">
+          <div class="row-span-1 md:row-span-5">
+            <h2 class="text-sm md:text-xl mb-4">September 25, 2025 | Saudi Arabia | Media mentions</h2>
+            <p class="text-black-700 font-bold md:text-4xl">
+          Asharq Al Awsat: السعودية تمضي مع «رؤية 2030» بدعم التصنيع والتوطين وتوسع الاقتصاد
+            </p>
+          </div>
+          <div class="row-span-1 md:row-span-1">
+            <a href="#" class="text-sky-500 hover:underline text-sm font-medium md:text-2xl inline-flex items-center">
+              Read more 
+              <span class="ml-1">→</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Individuals Section -->
+     <div class="py-4 md:py-8 col-span-1 md:col-span-2 border-t border-gray-300">
+        <div class="grid grid-rows-1 md:grid-rows-6 gap-y-2 md:gap-y-8">
+          <div class="row-span-1 md:row-span-5">
+            <h2 class="text-sm md:text-xl mb-4">September 25, 2025 | United Kingdom | Fragomen news</h2>
+            <p class="text-black-700 font-bold md:text-4xl">
+          Fragomen Spotlighted in Financial Times for 2025 Innovative Lawyers Europe Awards
+            </p>
+          </div>
+          <div class="row-span-1 md:row-span-1">
+            <a href="#" class="text-sky-500 hover:underline text-sm font-medium md:text-2xl inline-flex items-center">
+              Read more 
+              <span class="ml-1">→</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <!----------- Divider ----------->
+
+      <!-- Employers Section -->
+      <div class="py-4 md:py-8 col-span-1 md:col-span-2 border-t border-gray-300">
+        <div class="grid grid-rows-1 md:grid-rows-6 gap-y-2 md:gap-y-8">
+          <div class="row-span-1 md:row-span-5">
+            <h2 class="text-sm md:text-xl mb-4">September 26, 2025 | United States | Media mentions</h2>
+            <p class="text-black-700 font-bold md:text-4xl">
+            Business Insider: Trump’s $100,000 Visa Fee Threatens Wall Street’s Pipeline of Junior Bankers and...
+            </p>
+          </div>
+          <div class="row-span-1 md:row-span-1">
+            <a href="#" class="text-sky-500 hover:underline text-sm font-medium md:text-2xl inline-flex items-center">
+              Read more 
+              <span class="ml-1">→</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- Individuals Section -->
+      <div class="py-4 md:py-8 col-span-1 md:col-span-2 border-t border-gray-300">
+        <div class="grid grid-rows-1 md:grid-rows-6 gap-y-2 md:gap-y-8">
+          <div class="row-span-1 md:row-span-5">
+            <h2 class="text-sm md:text-xl mb-4">September 25, 2025 | Saudi Arabia | Media mentions</h2>
+            <p class="text-black-700 font-bold md:text-4xl">
+          Asharq Al Awsat: السعودية تمضي مع «رؤية 2030» بدعم التصنيع والتوطين وتوسع الاقتصاد
+            </p>
+          </div>
+          <div class="row-span-1 md:row-span-1">
+            <a href="#" class="text-sky-500 hover:underline text-sm font-medium md:text-2xl inline-flex items-center">
+              Read more 
+              <span class="ml-1">→</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Individuals Section -->
+     <div class="py-4 md:py-8 col-span-1 md:col-span-2 border-t border-gray-300">
+        <div class="grid grid-rows-1 md:grid-rows-6 gap-y-2 md:gap-y-8">
+          <div class="row-span-1 md:row-span-5">
+            <h2 class="text-sm md:text-xl mb-4">September 25, 2025 | United Kingdom | Fragomen news</h2>
+            <p class="text-black-700 font-bold md:text-4xl">
+          Fragomen Spotlighted in Financial Times for 2025 Innovative Lawyers Europe Awards
+            </p>
+          </div>
+          <div class="row-span-1 md:row-span-1">
+            <a href="#" class="text-sky-500 hover:underline text-sm font-medium md:text-2xl inline-flex items-center">
+              Read more 
+              <span class="ml-1">→</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <!----------- Divider ----------->
+    </div>
+  </div>
+</div>
